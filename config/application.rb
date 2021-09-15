722e703ef816688c1f5186a630bd99ce7e87a2bb
usman9012 <B-22006@student.usa.edu.pk>
2021-09-15 04:46:30 -0400

    css added

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module BugTracker
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.assets.precompile += %w[
-      user.css
+      user.css project.css
     ]
 
     # Settings in config/environments/* take precedence over those specified here.
